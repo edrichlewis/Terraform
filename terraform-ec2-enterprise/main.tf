d58bda73190beab336cdcd32d1e83d56dbe426fe
Edrich Lewis <105597780+edrichlewis@users.noreply.github.com>
2024-10-31 21:56:57 +0530

    Update main.tf

--- terraform-ec2-enterprise/main.tf
+++ terraform-ec2-enterprise/main.tf
@@ -8,6 +8,10 @@ data "aws_ami" "amazon_linux" {
     }
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 data "aws_subnet" "default" {
     vpc_id  = data.aws_vpc.default.id
     default = true

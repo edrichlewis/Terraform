bc1f465d8a2fbb6b4106fcf4e5ac1ff58b5c75ba
Edrich Lewis <105597780+edrichlewis@users.noreply.github.com>
2024-10-31 21:31:58 +0530

    Update backend.tf

--- terraform-ec2-enterprise/backend.tf
+++ terraform-ec2-enterprise/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = "your-enterprise-tf-state-bucket"
         key    = "ec2/terraform.tfstate"
-        region = "us-esat-2"
+        region = "us-east-2"
         dynamodb_table = "new-tf-table"
         encrypt = true
     }

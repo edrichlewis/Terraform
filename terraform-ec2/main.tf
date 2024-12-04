f827d0e7890baa5d723b5fd14f0d4f397d9f561c
Edrich Lewis <105597780+edrichlewis@users.noreply.github.com>
2024-12-04 23:56:55 +0530

    Update main.tf

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -1,10 +1,15 @@
 data "aws_subnet" "default" {
   default_for_az = true
+  availability_zone = "us-east-2a"
+}
+
+data "aws_vpc" "default" {
+  default = true  
 }
 
 module "ec2_instance" {
   source = "./modules/ec2"
-  ami = "ami-0453ec754f44f9a4a"
+  ami = "ami-0ea3c35c5c3284d82"
   instance_type = var.instance_type
   subnet_id = data.aws_subnet.default.id
   name = var.instance_name
